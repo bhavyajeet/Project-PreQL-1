c752d13a0b1e4079be21fb7608e2e2462b1d3157
Vikr-182 <vikrant.dewangan@research.iiit.ac.in>
2020-09-01 22:48:39 +0530

    OK

--- a.py
+++
@@ -1,8 +0,0 @@
-{
-	"m2m:uril": [   "~/in-cse/in-name/DHT", "~/in-cse/in-name/DHT/DATA", 
-                    "~/in-cse/in-name/DHT/DESCRIPTION", "~/in-cse/in-name/NovaPM", 
-                    "~/in-cse/in-name/NovaPM/DATA", "~/in-cse/in-name/NovaPM/DESCRIPTION", 
-                    "~/in-cse/in-name/MICS", "~/in-cse/in-name/MICS/DATA", "~/in-cse/in-name/MICS/DESCRIPTION", 
-                    "~/in-cse/in-name/Humidifier", "~/in-cse/in-name/Humidifier/DATA", 
-                    "~/in-cse/in-name/Humidifier/DESCRIPTION", "~/in-cse/in-name/Humidifier/COMMAND"]
-}
